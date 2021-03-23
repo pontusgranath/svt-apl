e173fc49fe3b7e7a17a01f0c550153dbfb5a6484
Felixxlll <felix.larsson@elev.ga.ntig.se>
2021-03-23 15:11:06 +0100

    Worked on form

--- django_project/dist_search/urls.py
+++ django_project/dist_search/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from django.conf.urls import url
 from . import views
 
 urlpatterns = [
     path('', views.home, name='dist_search-home'),
+    url(r'^calculate_distance', views.calculate_distance),
 ]
--- django_project/dist_search/views.py
+++ django_project/dist_search/views.py
@@ -1,5 +1,26 @@
 from django.shortcuts import render
 import pandas as pd
+import numpy as np
+import seaborn as sns
+from scipy.spatial.distance import pdist, squareform
+
+def calculate_distance(request):
+    data = pd.read_csv('Data-Table 1.csv', sep=';')
+    data.set_index('Client ID (ns_vid)', inplace=True)
+
+    m = np.matrix(data)
+    columns = list(data.columns)
+    res = squareform(pdist(m, 'hamming'))
+
+    def distance(t1, t2):
+        return res[columns.index(t1), columns.index(t2)]
+
+    search = request.POST('search-title')
+    d = pd.DataFrame([(c, distance(search,c)) for c in columns], columns=['title', 'distance']) 
+
+    sorted_values = d.sort_values('distance')
+
+    return sorted_values
 
 def home(request):
     data = pd.read_csv('Data-Table 1.csv', sep=';')
