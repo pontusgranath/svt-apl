02649ef1204c658d5976775f6baf8b24e4d379da
Felixxlll <felix.larsson@elev.ga.ntig.se>
2021-03-24 11:52:56 +0100

    Added home to views

--- django_project/dist_search/urls.py
+++ django_project/dist_search/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='dist_search-home'),
+    path('home/', views.home, name='dist_search-home'),
     url(r'^calculate_distance', views.calculate_distance),
 ]
