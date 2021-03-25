1e7dd389a824b97b380b3987e8f8404473fa09e1
Pontus Granath <pontus.granath@learnet.se>
2021-03-25 10:08:30 +0100

    updated shell script

--- source.sh
+++ source.sh
@@ -1,7 +1,9 @@
+#!/bin/bash
+
 if [ -d "venv" ]; then
-    source venv/bin/activate
-    
+    source "./venv/bin/activate"
+
     else
     virtualenv venv
-    source venv/bin/activate
+    source "./venv/bin/activate"
 fi
