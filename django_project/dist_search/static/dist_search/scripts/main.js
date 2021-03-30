ee24817fadb493c9cb1c57f728afbdf6d1c736bb
Felixxlll <felix.larsson@elev.ga.ntig.se>
2021-03-30 11:27:15 +0200

    Created beginning stages of inline search
    Co-authored-by: Felix Larsson <56984578+felixxlll@users.noreply.github.com>
    Co-authored-by: Pontus Granath <54897622+pontusgranath@users.noreply.github.com>

--- django_project/dist_search/static/dist_search/scripts/main.js
+++ django_project/dist_search/static/dist_search/scripts/main.js
@@ -14,19 +14,27 @@ $('#distance-button').click(function () {
     }
 })
 
-$(document).on('click', '.list-title', function() {    
-    $.ajax({
-        // url: '/calculate_distance',
-        method: 'POST', // or another (GET), whatever you need
-        data: {
-            // csrfmiddlewaretoken: "{{ csrf_token }}",
-            name: 'inline-search', // data you need to pass to your function
-            click: true
-        },
-        success: function (data) {
-            console.log('llama')
-            // success callback
-            // you can process data returned by function from views.py
-        }
-    });
-});
+$('.list-title').click(function(){
+    let searchedTitle = $(this).attr("show-title")
+    console.log(searchedTitle)
+
+    $('.inline-search-results').remove()
+    $(this).append("<div class='inline-search-results py-2 px-4'><h3>Search results for: <span>"+ searchedTitle +"</span></h3><ul><li>Titel 1</li><li>2 Titel</li><li>Titel 3</li><li>4 Titel</li><li>Titel 5</li></ul></div>")
+})
+
+// $(document).on('click', '.list-title', function() {    
+//     $.ajax({
+//         url: '/calculate_inline_distance',
+//         method: 'POST', // or another (GET), whatever you need
+//         data: {
+//             // csrfmiddlewaretoken: "{{ csrf_token }}",
+//             name: 'inline-search', // data you need to pass to your function
+//             click: true
+//         },
+//         success: function (data) {
+//             console.log('llama')
+//             // success callback
+//             // you can process data returned by function from views.py
+//         }
+//     });
+// });
--- django_project/dist_search/views.py
+++ django_project/dist_search/views.py
@@ -5,7 +5,34 @@ import seaborn as sns
 from scipy.spatial.distance import pdist, squareform
 from django.views.decorators.csrf import csrf_exempt
 
-@csrf_exempt
+def calculate_inline_distance(request):
+    data = pd.read_csv('Data-Table 1.csv', sep=';')
+    data.set_index('Client ID (ns_vid)', inplace=True)
+
+    pd.set_option('display.max_columns', None)
+    pd.set_option('display.max_rows', None)
+
+    matrix = np.matrix(data)
+    columns = list(data.columns)
+    res = squareform(pdist(matrix, 'hamming'))
+
+    def distance(t1, t2):
+        return res[columns.index(t1), columns.index(t2)]
+
+    search = request.POST.get('search-title')
+
+    dataframe = pd.DataFrame([(c, distance(search, c)) for c in columns], columns=['title', 'distance'])
+
+    sorted_values = dataframe.sort_values('distance')[:5 + 1]
+
+    context = {
+        'search': search,
+        'to_list': to_list,
+        'columns': columns
+    }
+
+    return render(request, 'dist_search/home.html', context)
+
 def calculate_distance(request):
     data = pd.read_csv('Data-Table 1.csv', sep=';')
     data.set_index('Client ID (ns_vid)', inplace=True)
