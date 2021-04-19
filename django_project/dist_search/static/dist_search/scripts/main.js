c3df678de55f1b7ba876960cbfe7e6be9f89ae7f
Felixxlll <felix.larsson@elev.ga.ntig.se>
2021-04-19 11:08:00 +0200

    Added code comments to main and index
    Co-authored-by: Pontus Granath <54897622+pontusgranath@users.noreply.github.com>

--- django_project/dist_search/static/dist_search/scripts/main.js
+++ django_project/dist_search/static/dist_search/scripts/main.js
@@ -1,8 +1,9 @@
+// Scrolls down to selected title on pageload
 try {
     const queryString = window.location.search
     const urlParams = new URLSearchParams(queryString);
     const currentInlineSearch = urlParams.get('inline-search-title')
-    
+
     $(document).ready(function() {
         window.location.href = '#' + currentInlineSearch
     })
@@ -11,9 +12,9 @@ catch {}
 
 let distanceButtonText = $('#distance-button').text()
 
+// Toggles text and border on toggle distance button
 $('#distance-button').click(function() {
     $('.distance-measurement').toggleClass(' show ')
-
 
     if ($('#distance-button').hasClass('activated')) {
         $('#distance-button').text(distanceButtonText)
