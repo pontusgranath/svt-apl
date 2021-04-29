469291cd383c900497e83092979d23cd806dede9
Felixxlll <felix.larsson@elev.ga.ntig.se>
2021-04-29 11:57:39 +0200

    Now reads from seperate distance table
    ie: no longer need to calculate distance every time page is loaded
    Co-Authored-By: Pontus <54897622+pontusgranath@users.noreply.github.com>

--- dist_search/views.py
+++ dist_search/views.py
@@ -11,30 +11,24 @@ def calculate_inline_distance(request):
     # Creates a connection to the database
     con = create_engine('postgresql://'+ config('DB_USER') +':'+ config('DB_PASSWORD') +'@'+ config('DB_HOST') +':5432/'+ config('DB_NAME') +'')
     # Gets the table named svt_statistics from the database
-    data = pd.read_sql('SELECT * FROM svt_statistics', con)
-    data.set_index('Client ID (ns_vid)', inplace=True)
+    data = pd.read_sql('SELECT * FROM distance_table', con)
 
-    matrix = np.matrix(data)
     titles = list(data.columns)
-    res = squareform(pdist(matrix, 'hamming'))
-
-    # Calculates distance between 2 titles
-    def distance(t1, t2):
-        return res[titles.index(t1), titles.index(t2)]
 
     # Gets search result by fetching from the form
     inline_search = request.GET.get('inline-search-title')
     search = request.GET.get('search-title')
+
     amount_of_titles = int(request.GET.get('title-amount'))
 
     # Uses search result to create a data table with its respective headers
-    inline_dataframe = pd.DataFrame([(c, distance(inline_search, c)) for c in titles], columns=['title', 'distance'])
-    dataframe = pd.DataFrame([(c, distance(search, c)) for c in titles], columns=['title', 'distance'])
+    inline_dataframe = data.filter(['Titles', inline_search], axis=1)
+    dataframe = data.filter(['Titles', search], axis=1)
 
     # Sorts titles and sets an upper limit on how many should be displayed
     # + 1 is used since the first title always gets cut due to it being the searched title
-    inline_sorted_titles = inline_dataframe.sort_values('distance')[:5 + 1]
-    sorted_titles = dataframe.sort_values('distance')[:amount_of_titles + 1]
+    inline_sorted_titles = inline_dataframe.sort_values(inline_search)[:5 + 1]
+    sorted_titles = dataframe.sort_values(search)[:amount_of_titles + 1]
 
     # Creates a list / array from the sorted titles
     inline_list_of_titles = inline_sorted_titles.values.tolist()
@@ -54,35 +48,25 @@ def calculate_inline_distance(request):
 def calculate_distance(request):
     con = create_engine('postgresql://'+ config('DB_USER') +':'+ config('DB_PASSWORD') +'@'+ config('DB_HOST') +':5432/'+ config('DB_NAME') +'')
     data = pd.read_sql('SELECT * FROM distance_table', con)
-    data.set_index('Titles', inplace=True)
 
-    # matrix = np.matrix(data)
     titles = list(data.columns)
-    # res = squareform(pdist(matrix, 'hamming'))
-
-    # def distance(t1, t2):
-    #     return res[titles.index(t1), titles.index(t2)]
 
     search = request.GET.get('search-title')
-    try:
-        # dataframe = pd.DataFrame([(c, distance(search, c)) for c in titles], columns=['title', 'distance'])
-        dataframe = data[['Agenda', search]]
-    except ValueError:
-        search = "Agenda"
-        dataframe = pd.DataFrame([(c, distance(search, c)) for c in titles], columns=['title', 'distance'])
+    
+    dataframe = data.filter(['Titles', search], axis=1)
 
     try:
         amount_of_titles = int(request.GET.get('title-amount'))
     except ValueError:
         amount_of_titles = 5
 
-    sorted_titles = dataframe.sort_values('distance')[:amount_of_titles + 1]
+    sorted_titles = dataframe.sort_values(search)[:amount_of_titles + 1]
 
     list_of_titles = sorted_titles.values.tolist()
 
     context = {
-        'titles': titles,
         'search': search,
+        'titles': titles,
         'title_amount': amount_of_titles,
         'list_of_titles': list_of_titles,
     }
@@ -92,7 +76,6 @@ def calculate_distance(request):
 def home(request):
     con = create_engine('postgresql://'+ config('DB_USER') +':'+ config('DB_PASSWORD') +'@'+ config('DB_HOST') +':5432/'+ config('DB_NAME') +'')
     data = pd.read_sql('SELECT * FROM distance_table', con)
-    data.set_index('Titles', inplace=True)
 
     titles = list(data.columns)
 
