24420bfdbf8f787c50945163180f1083c63504f6
Felixxlll <felix.larsson@elev.ga.ntig.se>
2021-05-05 11:30:39 +0200

    Made test that tests distance button functionality
    it works!
    
    Co-Authored-By: Pontus <54897622+pontusgranath@users.noreply.github.com>

--- tests/test_distance.py
+++ tests/test_distance.py
@@ -31,6 +31,37 @@ class Distance(unittest.TestCase):
         self.assertIn("Display distance", distanceButton.text)
         print("Third assertion finished!")
 
+    def test_distance_button_toggle_distance(self):
+        driver = self.driver
+        driver.get("http://localhost:8000")
+
+        searchField = driver.find_element_by_name('search-title')
+        searchField.send_keys("Klipp/simc")
+        searchField.send_keys(Keys.RETURN)
+
+        distanceButton = driver.find_element_by_id('distance-button')
+        distance = driver.find_elements_by_class_name('distance-measurement')
+        # Fails test if list is empty
+        self.assertIsNot(0, len(distance))
+        
+        # Checks if all elements are hidden
+        index = 0
+        for item in distance:
+            visibility = item.value_of_css_property("visibility")
+            self.assertEqual(visibility, "hidden")
+            index += 1
+            print("Item", index, "is hidden!")
+
+        distanceButton.click()
+
+        # Checks if all elements are visible after click
+        index = 0
+        for item in distance:
+            visibility = item.value_of_css_property("visibility")
+            self.assertEqual(visibility, "visible")
+            index += 1
+            print("Item", index, "is visible!")
+
     def tearDown(self):
         self.driver.close()
 
