39ae8e9a1c37b57678a604b01f1ce745e196bb1d
Pontus Granath <pontus.granath@learnet.se>
2021-05-06 10:17:37 +0200

    made test for inline-search results
    Co-authored-by: Felix Larsson <56984578+felixxlll@users.noreply.github.com>
    Co-authored-by: Pontus Granath <54897622+pontusgranath@users.noreply.github.com>

--- tests/test_inline_search_results.py
+++ tests/test_inline_search_results.py
@@ -8,23 +8,28 @@ class SearchResultsHeader(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Chrome()
 
+    # Checks if the results in inline-search is correct
     def test_correct_inline_search_results(self):
         driver = self.driver = webdriver.Chrome()
         driver.get("http://localhost:8000")
 
-        searchField = driver.find_element_by_name('search-title')
-        searchField.send_keys('Klipp/simc')
-        searchField.send_keys(Keys.RETURN)
+        def CompareResults(title, amount, expectedResults):
+            searchField = driver.find_element_by_name('search-title')
+            searchField.send_keys(title)
+            searchField.send_keys(Keys.RETURN)
 
-        inlineButton = driver.find_element_by_class_name('plus-button')
-        inlineButton.click()
-
-        time.sleep(2)
+            inlineButtons = driver.find_elements_by_class_name('plus-button')
+            inlineButtons[amount].click()
         
+            inlineList = driver.find_elements_by_xpath('//span[@class="inline_distance"]/parent::li')
+            inlineSearchResults = [element.text for element in inlineList]
+            inlineSearchResults = [i.split(' - ', 1)[0] for i in inlineSearchResults]
 
-        x = driver.find_element_by_xpath('//span[@class="inline_distance"]/parent::li')
-        print('Element Found')
+            self.assertListEqual(expectedResults, inlineSearchResults)
 
+        CompareResults('Klipp/simc', 0, ['Bolibompa: Drakens trädgård', 'Kalifat', 'Skidskytte: VM', 'Melodifestivalen 2020: Deltävling 3', 'Svenska nyheter'])
+        CompareResults('Klipp/simc', 1, ['Kalifat', 'Melodifestivalen 2020: Deltävling 3', 'Falkenberg forever', 'Svenska nyheter', 'Stjärnorna på slottet'])
+        CompareResults('Bolibompa: Drakens trädgård', 4, ['Kalifat', 'Pippi Långstrump', 'Skidskytte: VM', 'Falkenberg forever', 'Svenska nyheter'])
 
 
     def tearDown(self):
